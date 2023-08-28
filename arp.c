cd5cee1a7d2d44f90c34b36e293dfe3790fc64ff
hihumikan <a@qqey.net>
2023-08-28 18:20:11 +0900

    feat:

--- arp.c
+++ arp.c
@@ -99,26 +99,111 @@ arp_dump(const uint8_t *data, size_t len)
 static void
 arp_cache_delete(struct arp_cache *cache)
 {
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[ETHER_ADDR_STR_LEN];
+
+    debugf("DELETE: pa=%s, ha=%s", ip_addr_ntop(cache->pa, addr1, sizeof(addr1)), ether_addr_ntop(cache->ha, addr2, sizeof(addr2)));
+    
+    // キャッシュのエントリを削除する
+    // ・state は未使用（FREE）の状態にする
+    // ・各フィールドを 0 にする
+    // ・timestamp は timerclear() でクリアする
+
+    cache->state = ARP_CACHE_STATE_FREE;
+
+    cache->pa = 0;
+    memset(cache->ha, 0, ETHER_ADDR_LEN);
+
+    timerclear(&cache->timestamp);
 }
 
 static struct arp_cache *
 arp_cache_alloc(void)
 {
+    struct arp_cache *entry, *oldest = NULL;
+
+    for (entry = caches; entry < tailof(caches); entry++) {
+        if (entry->state == ARP_CACHE_STATE_FREE) {
+            return entry;
+        }
+        if (!oldest || timercmp(&oldest->timestamp, &entry->timestamp, >)) {
+            oldest = entry;
+        }
+    }
+    arp_cache_delete(oldest);
+    return oldest;
+
 }
 
 static struct arp_cache *
 arp_cache_select(ip_addr_t pa)
 {
+    //キャッシュの中からプロトコルアドレスが一致するエントリを探して返す
+    // ・念のため FREE 状態ではないエントリの中から探す
+    // ・見つからなかったら NULL を返す
+
+    struct arp_cache *entry;
+
+    for (entry = caches; entry < tailof(caches); entry++) {
+        if (entry->state != ARP_CACHE_STATE_FREE && entry->pa == pa) {
+            return entry;
+        }
+    }
+    return NULL;
 }
 
 static struct arp_cache *
 arp_cache_update(ip_addr_t pa, const uint8_t *ha)
 {
+    struct arp_cache *cache;
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[ETHER_ADDR_STR_LEN];
+
+    // キャッシュに登録されている情報を更新する
+    // (1) arp_cache_select() でエントリを検索する
+    // ・見つからなかったらエラー（NULL）を返す
+    // (2) エントリの情報を更新する
+    // ・state は 解決済み（RESOLVED）の状態にする
+    // ・timestamp は gettimeofday() で設定する ※ 使い方がわからなかったら調べること
+    cache = arp_cache_select(pa);
+    if (!cache) {
+        /* not found */
+        return NULL;
+    }
+
+    cache->state = ARP_CACHE_STATE_RESOLVED;
+    memcpy(cache->ha, ha, ETHER_ADDR_LEN);
+    gettimeofday(&cache->timestamp, NULL);
+    debugf("UPDATE: pa=%s, ha=%s", ip_addr_ntop(pa, addr1, sizeof(addr1)), ether_addr_ntop(ha, addr2, sizeof(addr2)));
+    return cache;
 }
 
 static struct arp_cache *
 arp_cache_insert(ip_addr_t pa, const uint8_t *ha)
 {
+    struct arp_cache *cache;
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[ETHER_ADDR_STR_LEN];
+
+
+    // キャッシュに新しくエントリを登録する
+    // (1) arp_cache_alloc() でエントリの登録スペースを確保する
+    // ・確保できなかったらエラー（NULL）を返す
+    // (2) エントリの情報を設定する
+    // ・state は 解決済み（RESOLVED）の状態にする
+    // ・timestamp は gettimeofday() で設定する ※ 使い方がわからなかったら調べること
+
+    cache = arp_cache_alloc();
+    if (!cache) {
+        errorf("arp_cache_alloc() failure");
+        return NULL;
+    }
+    cache->state = ARP_CACHE_STATE_RESOLVED;
+    cache->pa = pa;
+    memcpy(cache->ha, ha, ETHER_ADDR_LEN);
+    gettimeofday(&cache->timestamp, NULL);
+    debugf("INSERT: pa=%s, ha=%s", ip_addr_ntop(pa, addr1, sizeof(addr1)), ether_addr_ntop(ha, addr2, sizeof(addr2)));
+    return cache;
 }
 
 static int
@@ -146,6 +231,7 @@ arp_input(const uint8_t *data, size_t len, struct net_device *dev)
     struct arp_ether_ip *msg;
     ip_addr_t spa, tpa;
     struct net_iface *iface;
+    int marge = 0;
 
     if (len < sizeof(*msg)) {
         errorf("too short");
@@ -164,8 +250,21 @@ arp_input(const uint8_t *data, size_t len, struct net_device *dev)
     arp_dump(data, len);
     memcpy(&spa, msg->spa, sizeof(spa));
     memcpy(&tpa, msg->tpa, sizeof(tpa));
+    mutex_lock(&mutex);
+    if (arp_cache_update(spa, msg->sha)) {
+        /* updated */
+        marge = 1;
+    }
+    mutex_unlock(&mutex);
+
     iface = net_device_get_iface(dev, NET_IFACE_FAMILY_IP);
     if (iface && ((struct ip_iface *)iface)->unicast == tpa) {
+        if (!marge) {
+            mutex_lock(&mutex);
+            arp_cache_insert(spa, msg->sha);
+            mutex_unlock(&mutex);
+        }
+
         if (ntoh16(msg->hdr.op) == ARP_OP_REQUEST) {
             arp_reply(iface, msg->sha, spa, msg->sha);
         }
@@ -175,6 +274,30 @@ arp_input(const uint8_t *data, size_t len, struct net_device *dev)
 int
 arp_resolve(struct net_iface *iface, ip_addr_t pa, uint8_t *ha)
 {
+    struct arp_cache *cache;
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[ETHER_ADDR_STR_LEN];
+
+    if (iface->dev->type != NET_DEVICE_TYPE_ETHERNET) {
+        debugf("unsupported hardware address type");
+        return ARP_RESOLVE_ERROR;
+    }
+    if (iface->family != NET_IFACE_FAMILY_IP) {
+        debugf("unsupported protocol address type");
+        return ARP_RESOLVE_ERROR;
+    }
+    mutex_lock(&mutex);
+    cache = arp_cache_select(pa);
+    if (!cache) {
+        debugf("cache not found, pa=%s", ip_addr_ntop(pa, addr1, sizeof(addr1)));
+        mutex_unlock(&mutex);
+        return ARP_RESOLVE_ERROR;
+    }
+    memcpy(ha, cache->ha, ETHER_ADDR_LEN);
+    mutex_unlock(&mutex);
+    debugf("resolved, pa=%s, ha=%s",
+        ip_addr_ntop(pa, addr1, sizeof(addr1)), ether_addr_ntop(ha, addr2, sizeof(addr2)));
+    return ARP_RESOLVE_FOUND;
 }
 
 int
--- ip.c
+++ ip.c
@@ -10,6 +10,8 @@
 #include "util.h"
 #include "net.h"
 #include "ip.h"
+#include "arp.h"
+
 
 struct ip_hdr {
     uint8_t vhl;
@@ -254,13 +256,18 @@ static int
 ip_output_device(struct ip_iface *iface, const uint8_t *data, size_t len, ip_addr_t dst)
 {
     uint8_t hwaddr[NET_DEVICE_ADDR_LEN] = {};
-
+    int ret;
     if (NET_IFACE(iface)->dev->flags & NET_DEVICE_FLAG_NEED_ARP) {
         if (dst == iface->broadcast || dst == IP_ADDR_BROADCAST) {
             memcpy(hwaddr, NET_IFACE(iface)->dev->broadcast, NET_IFACE(iface)->dev->alen);
         } else {
-            errorf("arp does not implement");
-            return -1;
+            //arp_resolve() を呼び出してアドレスを解決する
+            // ・戻り値が ARP_RESOLVE_FOUND でなかったらその値をこの関数の戻り値として返す
+
+            ret = arp_resolve(NET_IFACE(iface), dst, hwaddr);
+            if (ret != ARP_RESOLVE_FOUND) {
+                return ret;
+            }
         }
     }
     return net_device_output(NET_IFACE(iface)->dev, NET_PROTOCOL_TYPE_IP, data, len, hwaddr);
